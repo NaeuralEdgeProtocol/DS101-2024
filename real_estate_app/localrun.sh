fbed7c0f97ef0a34e96f1c96f92fc636c37dc274
Andrei Ionut Damian <andrei.damian@me.com>
2024-11-18 18:06:15 +0200

    lecture 6 & workshop

---
+++ real_estate_app/localrun.sh
@@ -0,0 +1,3 @@
+cd src/
+uvicorn main:app --host 0.0.0.0 --port 5050
+cd ..
--- real_estate_app/src/main.py
+++ real_estate_app/src/main.py
@@ -1,6 +1,24 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+
+if os.path.isdir("models"):
+  MODEL_PATH = "models/linear_model_nb_v1.npy"                
+elif os.path.isdir("../models"):
+  MODEL_PATH = "../models/linear_model_nb_v1.npy"
+else:
+  raise Exception("Could not find the models directory")
+
+
+def log_message(message):
+  print("MYLOG:", message, flush=True)
+
+# now we load the linear model
+import numpy as np
+model_data = np.load(MODEL_PATH, allow_pickle=True).item()
+log_message(f"LOADED MODEL DATA: {model_data}")
+
 
 
 class AddressInputData(BaseModel):
@@ -29,6 +47,24 @@ def dummy_model(data: AddressInputData):
   return inferred_price
 
 
+def linear_model(data: AddressInputData):
+  input_data = []
+  for field in model_data["field_mapping"]:
+    input_data.append(getattr(data, field))
+  np_x = np.array(input_data).reshape(1, -1)
+  log_message(f"Input data: {np_x}")
+  np_x_mean, np_x_std = model_data["input_transform"]
+  np_y_min, np_y_max = model_data["output_transform"]
+  np_x_n = (np_x - np_x_mean) / np_x_std
+  np_x_nb = np.concatenate([np_x_n, np.ones((np_x_n.shape[0], 1))], axis=1)
+  np_w = model_data["weights"]
+  log_message(f"Normalized with bias inputs: {np_x_nb}")
+  np_yh = np_x_nb.dot(np_w)
+  inferred_price = np_yh * (np_y_max - np_y_min) + np_y_min
+  log_message(f"Inferred price: {inferred_price}")
+  return round(inferred_price[0],2)
+
+
 # new we prepare the root HTML page
 @app.get("/", response_class=HTMLResponse)
 async def show_html(request: Request):
@@ -44,5 +80,8 @@ async def show_html(request: Request):
 # the appraise endpoint will receive the data from the form and return the inferred price
 @app.post("/appraise")
 async def appraise(data:  AddressInputData):
-  inferred_price = dummy_model(data)
+  """
+  This endpoint receives the data from the form and returns the inferred price
+  """
+  inferred_price = linear_model(data)
   return {"inferred_price": inferred_price}
